22cc8d92c1086e78628cff2d97aa72c3248939fc
MariusLeChapelier <mariuslechapelier@gmail.com>
2024-12-03 18:07:27 +0100

    test autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath(os.path.join("../src/simple_retico_agent")))
 sys.path.insert(0, os.path.abspath(os.path.join("../src")))
 sys.path.insert(0, os.path.abspath(".."))
 
